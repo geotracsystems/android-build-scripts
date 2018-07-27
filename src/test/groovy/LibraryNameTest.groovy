91d13fb437690be8cb97a42cd956888e84584707
Andy Wang <andy_wang@trimble.com>
2018-07-27 10:17:05 -0600

    return empty publish URL when in HEAD branch, so that the build will be success but would NOT allow publish

--- src/test/groovy/LibraryNameTest.groovy
+++ src/test/groovy/LibraryNameTest.groovy
@@ -233,7 +233,6 @@ class LibraryNameTest extends Specification {
         baseVersion << ['1', 'MOCOM-353', 'Test.Version', '1.Something.0', '1.2.3.4']
     }
 
-    @Unroll
     def "Library version is correct for arbitrary commit in HEAD branch: #HEAD"() {
         given:
         setVersion('1.2')
